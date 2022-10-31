b8fd2e653c20ccbfbe3151e2ec71a44ecafc46b7
Molnár Bálint <molnarbalintattila@gmail.com>
2022-10-31 16:31:12 +0100

    form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,20 @@
 
 @section('content')
    
-    <div class="container">
+    <div class="container bg-dark text-white rounded p-3 mx-auto">
         <div class="row">
-            <div class="col">
-                
+            <div class="col-6">
+                <form method="_POST">
+                    @csrf
+                    <h1 >Adatok:</h1>
+                    <div class="my-3">
+                    <label for="vnev">Vezetéknév:</label><input type="text" name="vnev" id="vnev" class="form-control text-center" ></div>
+                    <div class="my-3">
+                    <label for="knev">Keresztnév:</label> <input type="text" name="knev" id="knev" class="form-control text-center"></div>
+                    <div class="my-3">
+                    <label for="szul">Születési év:</label><input type="date" name="szul" id="szul" class="form-control text-center"></div>
+                    <div class="col-md-12 text-center"><button type="submit" class="btn btn-rounded btn-primary form-control" >Beküld</button></div>
+                </form>
             </div>
         </div>
     </div>
