121bbbde84f2c7f8c58d3f9a4e08898a663efb20
Molnár Bálint <molnarbalintattila@gmail.com>
2022-10-31 17:16:31 +0100

    Validáció kész

--- app/Http/Controllers/Elso.php
+++ app/Http/Controllers/Elso.php
@@ -14,29 +14,32 @@ class Elso extends Controller
    }
 
    public function rogzites(Request $req){
+    $req->validate(
+        [
+            "vnev"=>"required|min:1|max:30",
+            "knev"=>"required|min:1|max:30",
+            "szul"=>"required|date|max:".(date("Y")),
 
-            $req->validate(
-                [
-                    "vnev"=>"required|min:1|max:30",
-                    "knev"=>"required|min:1|max:30",
-                    "szul"=>"required|date|max:".(date("Y")),
+        ],
 
-                ],
+        [
+            "vnev.required"=>"A mezőt kötelező kitölteni!",
+            "vnev.min"=> "Minimum 1 karakter!",
+            "vnev.max"=> "maximum 30 karakter!",
 
-                [
-                    "vnev.required"=>"A mezőt kötelező kitölteni!",
-                    "vnev.min"=> "Minimum 1 karakter!",
-                    "vnev.max"=> "maximum 30 karakter!",
+            "knev.required"=>"A mezőt kötelező kitölteni!",
+            "knev.min"=> "Minimum 1 karakter!",
+            "knev.max"=> "maximum 30 karakter!",
 
-                    "knev.required"=>"A mezőt kötelező kitölteni!",
-                    "knev.min"=> "Minimum 1 karakter!",
-                    "knev.max"=> "maximum 30 karakter!",
+            "szul.required" => "kötelező kitölteni a mezőt!",
+            "szult.date" => "Csak dátum lehet!",
+            "szul.max" =>"A mai dátumnál nem lehet nagyobb"
+        ] 
 
-                    "szul.required" => "kötelező kitölteni a mezőt!",
-                    "szult.date" => "Csak dátum lehet!",
-                    "szul.max" =>"A mai dátumnál nem lehet nagyobb"
-                ]
-            );
+        );
+
+
+
    }
 
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,37 @@
     <div class="container bg-dark text-white rounded p-3 mx-auto">
         <div class="row">
             <div class="col-6">
-                <form method="_POST">
+                <form method="POST">
                     @csrf
                     <h1 >Adatok:</h1>
                     <div class="my-3">
-                    <label for="vnev">Vezetéknév:</label><input type="text" name="vnev" id="vnev" class="form-control text-center" ></div>
+                    <label for="vnev">Vezetéknév:</label><input type="text" name="vnev" id="vnev" class="form-control text-center" >
+                    @error('vnev')
+                        <div class="alert alert-danger">
+                            {{$message}}
+                        </div>
+                    @enderror
+                    </div>
                     <div class="my-3">
-                    <label for="knev">Keresztnév:</label> <input type="text" name="knev" id="knev" class="form-control text-center"></div>
+                    <label for="knev">Keresztnév:</label> <input type="text" name="knev" id="knev" class="form-control text-center">
+                
+                    @error('knev')
+                    <div class="alert alert-danger">
+                        {{$message}}
+                    </div>
+                     @enderror
+                    </div>
                     <div class="my-3">
-                    <label for="szul">Születési év:</label><input type="date" name="szul" id="szul" class="form-control text-center"></div>
-                    <div class="col-md-12 text-center"><button type="submit" class="btn btn-rounded btn-primary form-control" >Beküld</button></div>
+                    <label for="szul">Születési év:</label><input type="date" name="szul" id="szul" class="form-control text-center">
+                        
+                    @error('szul')
+                    <div class="alert alert-danger">
+                        {{$message}}
+                    </div>
+                     @enderror
+                    
+                    </div>
+                    <div><button type="submit" class="btn btn-primary ">Bekuldes</button></div>
                 </form>
             </div>
         </div>
